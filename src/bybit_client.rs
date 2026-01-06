16b3bb29f45144728b39819bc5401d289fb68ab5
vadym <vadim.bobrovskij@gmail.com>
2026-01-06 09:00:47 +0000

    token order added

--- src/bybit_client.rs
+++ src/bybit_client.rs
@@ -1,4 +1,6 @@
 use serde::Deserialize;
+use std::collections::HashMap;
+
 
 pub const BYBIT_WS_URL: &str = "https://api.bybit.com/v5/market/tickers?category=spot";
 
@@ -6,7 +8,6 @@ pub const SYMBOLS: [&str; 10] = [
     "BTCUSDT",  "ETHUSDT",   "SOLUSDT", "XRPUSDT",  "TRXUSDT",
     "ENAUSDT",  "ETHFIUSDT", "APTUSDT", "STRKUSDT", "ASTERUSDT"  
 ];
-
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -22,7 +23,6 @@ pub struct V5Result<T> {
     list: T, 
 }
 
-
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TickerInfo {
@@ -32,23 +32,34 @@ pub struct TickerInfo {
     ask1_price: String,
 }
 
-
 pub async fn fetch_tickers() -> String {
     let response = reqwest::get(BYBIT_WS_URL).await;
 
     match response {
         Ok(resp) => {
             let data = resp.json::<BybitResponseV5<Vec<TickerInfo>>>().await;
+            
             match data {
                 Ok(res) if res.ret_code == 0 => {
-                    let mut output = String::from("Symbol | Price | Bid | Ask\n");
-                    output.push_str("----------------------------------\n");
+                    let ticker_map: HashMap<String, TickerInfo> = res.result.list
+                        .into_iter()
+                        .map(|t| (t.symbol.clone(), t))
+                        .collect();
                     
-                    for t in res.result.list.iter().filter(|t| SYMBOLS.contains(&t.symbol.as_str())) {
-                        output.push_str(&format!(
-                            "{:<10} | {:<10} | {:<10} | {:<10}\n", 
-                            t.symbol, t.last_price, t.bid1_price, t.ask1_price
-                        ));
+                    let mut output = String::from(format!(
+                        "{:<14} | {:<18} | {:<18} | {:<18}\n", 
+                        "Symbol", "Price", "Bid", "Ask"
+                    ));
+                    
+                    output.push_str("----------------------------------------------------------------------------\n");
+                    
+                    for symbol in SYMBOLS.iter() {
+                        if let Some(t) = ticker_map.get(*symbol) {
+                            output.push_str(&format!(
+                                "{:<12} | {:<18} | {:<18} | {:<18}\n", 
+                                t.symbol, t.last_price, t.bid1_price, t.ask1_price
+                            ));
+                        }
                     }
                     output
                 }
@@ -59,45 +70,3 @@ pub async fn fetch_tickers() -> String {
         Err(e) => format!("Connection error: {}", e),
     }
 }
-
-// pub async fn run_client() -> Result<(), Error> {
-
-//     let response = reqwest::get(BYBIT_WS_URL)
-//         .await?
-//         .json::<BybitResponseV5<Vec<TickerInfo>>>() 
-//         .await?;
-
-//     match response.ret_code {
-//         0 => {
-//             println!("\n✅ Successfully retrieved the list of Tickers V5:");
-            
-//             let all_tickets = response.result.list;
-
-//             let filtered_tickers: Vec<TickerInfo> = all_tickets
-//                 .into_iter()
-//                 .filter(|ticker_info| {
-//                     SYMBOLS.contains(&ticker_info.symbol.as_str())
-//                 })
-//                 .collect();
-            
-
-//             println!("{:<5} {:<10} {:<20} {:<20} {:<20}", "ID", "Symbol", "Last Price", "Bid (Buy)", "Ask (Sell)");
-//             println!("---------------------------------------------------------------------------------");
-
-//             for (i, ticker) in filtered_tickers.iter().enumerate() {
-//                 println!("{:<5} {:<10} {:<20} {:<20} {:<20}",
-//                     i + 1,
-//                     ticker.symbol,
-//                     ticker.last_price,
-//                     ticker.bid1_price,
-//                     ticker.ask1_price
-//                 );
-//             }
-//         }
-//         _ => {
-//             eprintln!("❌ API V5 Error (Code: {}): {}", response.ret_code, response.ret_msg);
-//         }
-//     }
-
-//     Ok(())
-// }
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,9 @@ mod gui;
 mod bybit_client;
 
 use gui::MyWindow;
-use iced::{Settings, Application}; // Импортируем Application
+use iced::{Settings, Application};
 
 #[tokio::main]
 async fn main() -> iced::Result {
     MyWindow::run(Settings::default())
 }
-
-
-
-// use bybit_client::run_client;
-// use reqwest::Error;
-
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-
-//     MyWindow::run(Settings::default())
-//     //run_client().await?;
-
-//     Ok(())
-// }
- 
